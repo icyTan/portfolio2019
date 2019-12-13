a62dba1b6d16cb26297b83006e191de85ae59e6f
Patrik Lau <get.patlau@gmail.com>
2019-12-12 22:59:31 -0800

    Small fixes for Z-index based on last commit, landing page animations started

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,21 +11,23 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Portfolio" />
     <Header barType="index"/>
-    <section className="landing-work wrap-1 sf-1 fullpage-fade-in">
-      <h2 className="vis-hide">Patrik Lau</h2>
-      <h1 className="highlight">I'm Pat,</h1>
-      <p className="display-text highlight">
-        a growing product designer in Vancouver, BC.
-      </p>
-      <p className="display-text secondary-color">
-       I like to tinker with prototyping tools, have an intense curiousity of design systems, and a love for baking.
-      </p>
-      {/* <p className="display-text secondary-color">
+    <section className="landing-work wrap-1 sf-1">
+      <div className="landing-fade-in">
+        <h2 className="vis-hide">Patrik Lau</h2>
+        <h1 className="highlight">I'm Pat,</h1>
+        <p className="display-text highlight">
+          a growing product designer in Vancouver, BC.
+        </p>
+        <p className="display-text secondary-color">
+          I like to tinker with prototyping tools, have an intense curiousity of design systems, and a love for baking.
+        </p>
+        {/* <p className="display-text secondary-color">
         I believe that good design is effective communication with the people on the other side of my work.
-      </p> */}
-      <p className="display-text">
-        This portfolio is built with <a href="https://www.gatsbyjs.org/">Gatsby.js</a> and is powered by <a href="https://www.netlify.com">Netlify</a>.
-      </p>
+        </p> */}
+        <p className="display-text">
+            This portfolio is built with <a href="https://www.gatsbyjs.org/">Gatsby.js</a> and is powered by <a href="https://www.netlify.com">Netlify</a>.
+        </p>
+      </div>
     </section>
     <Landing />
   </Layout>
